b3ed8e3fe6babb4e0ef53d666d3f9ef0145bbe03
MoskalykA <45717749+MoskalykA@users.noreply.github.com>
2021-08-06 13:04:29 +0200

    🚧 Nuget lib

---
+++ LazyCsharp/Class1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+
+namespace LazyCsharp
+{
+    class Callback
+    {
+        public Action<Newtonsoft.Json.Linq.JToken> success;
+        public Action<Newtonsoft.Json.Linq.JToken> fail;
+    }
+    class Database
+    {
+        private string addr;
+        private int port;
+        private int id = 0;
+        private Dictionary<int, Callback> callbacks = new Dictionary<int, Callback> { };
+        private WebSocket ws;
+        private List<string> messageQueue = new List<string> { };
+
+        public class ReceiveObject
+        {
+            public bool s { get; set; }
+            public int id { get; set; }
+            public object r { get; set; }
+        }
+
+        public Database(string addr, int port, [Optional] Action<object> onconnect)
+        {
+            this.addr = addr;
+            this.port = port;
+            this.ws = new WebSocket($"ws://{addr}:{port}");
+            this.ws.OnOpen += (sender, e) =>
+            {
+                onconnect(e);
+            };
+
+            this.ws.OnMessage += (sender, e) =>
+            {
+                var messages = e.Data.Split("|");
+                for (int i = 0; i < messages.Length; i++)
+                {
+                    var msg = messages[i];
+                    Newtonsoft.Json.Linq.JObject receivedMessage = JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JObject>(msg);
+                    if (receivedMessage.Value<bool>("s"))
+                    {
+                        this.callbacks[receivedMessage.Value<int>("id")].success.Invoke(receivedMessage.GetValue("r"));
+                    }
+                    else
+                    {
+                        this.callbacks[receivedMessage.Value<int>("id")].fail.Invoke(receivedMessage.GetValue("r"));
+                    }
+                }
+            };
+
+            Task task = SetInterval(sendQueue, TimeSpan.FromSeconds(0.1));
+
+            this.ws.Connect();
+        }
+
+        public void sendQueue()
+        {
+            if (this.messageQueue.Count > 0 && this.ws.ReadyState.ToString() == "Open")
+            {
+                this.ws.Send(string.Join("|", this.messageQueue));
+            }
+
+            this.messageQueue = new List<string> { };
+        }
+
+        [DataContract]
+        public class Send
+        {
+            [DataMember(Name = "c")]
+            public string c { get; set; }
+
+            [DataMember(Name = "id")]
+            public int id { get; set; }
+
+            [DataMember(Name = "a")]
+            public object a { get; set; }
+        }
+
+        public void send(string name, Object args, Callback callback)
+        {
+            int id = ++this.id;
+            Send send = new Send
+            {
+                c = name,
+                id = id,
+                a = args
+            };
+
+            string message = JsonConvert.SerializeObject(send);
+            if (!message.Contains("|"))
+            {
+                this.messageQueue.Add(message);
+                this.callbacks.Add(id, callback);
+            }
+        }
+
+        public void connect(string email, string password, Callback callback)
+        {
+            Dictionary<string, string> args = new Dictionary<string, string> { };
+            args.Add("email", email);
+            args.Add("password", password);
+
+            this.send("connect", args, callback);
+        }
+
+        public void register(string email, string password, string username, string full_name, Callback callback)
+        {
+            Dictionary<string, string> args = new Dictionary<string, string> { };
+            args.Add("email", email);
+            args.Add("password", password);
+            args.Add("username", username);
+            args.Add("full_name", full_name);
+
+            this.send("register", args, callback);
+        }
+
+        public void create(string keyPath, Newtonsoft.Json.Linq.JObject value, Callback callback)
+        {
+            var key = keyPath.Split("/");
+
+            Newtonsoft.Json.Linq.JObject args = new Newtonsoft.Json.Linq.JObject { };
+            args.Add("keyPath", Newtonsoft.Json.Linq.JToken.FromObject(key));
+            args.Add("value", value);
+            args.Add("w", true);
+
+            this.send("create", args, callback);
+        }
+
+        public void get(string keyPath, Callback callback)
+        {
+            var key = keyPath.Split("/");
+
+            Newtonsoft.Json.Linq.JObject args = new Newtonsoft.Json.Linq.JObject { };
+            args.Add("keyPath", Newtonsoft.Json.Linq.JToken.FromObject(key));
+
+            this.send("get", args, callback);
+        }
+
+        public void delete(string keyPath, Callback callback)
+        {
+            var key = keyPath.Split("/");
+
+            Newtonsoft.Json.Linq.JObject args = new Newtonsoft.Json.Linq.JObject { };
+            args.Add("keyPath", Newtonsoft.Json.Linq.JToken.FromObject(key));
+
+            this.send("delete", args, callback);
+        }
+
+        public void keys(string keyPath, string filter, Callback callback)
+        {
+            var key = keyPath.Split("/");
+
+            Newtonsoft.Json.Linq.JObject args = new Newtonsoft.Json.Linq.JObject { };
+            args.Add("keyPath", Newtonsoft.Json.Linq.JToken.FromObject(key));
+            args.Add("filter", filter);
+
+            this.send("keys", args, callback);
+        }
+
+        public void update(string keyPath, string value, Callback callback)
+        {
+            var key = keyPath.Split("/");
+
+            Newtonsoft.Json.Linq.JObject args = new Newtonsoft.Json.Linq.JObject { };
+            args.Add("keyPath", Newtonsoft.Json.Linq.JToken.FromObject(key));
+            args.Add("value", value);
+            args.Add("w", true);
+
+            this.send("update", args, callback);
+        }
+
+        public void exists(string keyPath, Callback callback)
+        {
+            var key = keyPath.Split("/");
+
+            Newtonsoft.Json.Linq.JObject args = new Newtonsoft.Json.Linq.JObject { };
+            args.Add("keyPath", Newtonsoft.Json.Linq.JToken.FromObject(key));
+
+            this.send("exist", args, callback);
+        }
+
+        public void size(string keyPath, Callback callback)
+        {
+            var key = keyPath.Split("/");
+
+            Newtonsoft.Json.Linq.JObject args = new Newtonsoft.Json.Linq.JObject { };
+            args.Add("keyPath", Newtonsoft.Json.Linq.JToken.FromObject(key));
+
+            this.send("size", args, callback);
+        }
+
+        public static async Task SetInterval(Action action, TimeSpan timeout)
+        {
+            await Task.Delay(timeout).ConfigureAwait(false);
+
+            action();
+
+            await SetInterval(action, timeout);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,56 @@
+<br/>
+<p align="center">
+  <a href="https://github.com/LazyDB-community/LazyCSharp">
+    <img src="https://i.imgur.com/oTW6Hab.png" alt="Logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">LazyCSharp</h3>
+
+  <p align="center">
+    Connector for LazyDB in CSharp.
+    <br/>
+    <br/>
+    <a href="https://github.com/LazyDB-community/LazyCSharp"><strong>Explore the docs »</strong></a>
+    <br/>
+    <br/>
+    <a href="https://github.com/LazyDB-community/LazyCSharp">View Demo</a>
+    .
+    <a href="https://github.com/LazyDB-community/LazyCSharp/issues">Report Bug</a>
+    .
+    <a href="https://github.com/LazyDB-community/LazyCSharp/issues">Request Feature</a>
+  </p>
+</p>
+
+![Downloads](https://img.shields.io/github/downloads/LazyDB-community/LazyCSharp/total) ![Contributors](https://img.shields.io/github/contributors/LazyDB-community/LazyCSharp?color=dark-green) ![Forks](https://img.shields.io/github/forks/LazyDB-community/LazyCSharp?style=social) ![Stargazers](https://img.shields.io/github/stars/LazyDB-community/LazyCSharp?style=social) ![Issues](https://img.shields.io/github/issues/LazyDB-community/LazyCSharp) ![License](https://img.shields.io/github/license/LazyDB-community/LazyCSharp) 
+
+## Table Of Contents
+
+* [Contributing](#contributing)
+* [License](#license)
+* [Authors](#authors)
+* [Acknowledgements](#acknowledgements)
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+* If you have suggestions for adding or removing projects, feel free to [open an issue](https://github.com/LazyDB-community/LazyCSharp/issues/new) to discuss it, or directly create a pull request after you edit the *README.md* file with necessary changes.
+* Please make sure you check your spelling and grammar.
+* Create individual PR for each suggestion.
+* Please also read through the [Code Of Conduct](https://github.com/LazyDB-community/LazyCSharp/blob/main/CODE_OF_CONDUCT.md) before posting your first idea as well.
+
+### Creating A Pull Request
+
+1. Fork the Project
+2. Create your Feature Branch
+3. Commit your Changes
+4. Push to the Branch
+5. Open a Pull Request
+
+## License
+
+Distributed under the MIT License. See [LICENSE](https://github.com/LazyDB-community/LazyCSharp/blob/main/LICENSE.md) for more information.
+
+## Authors
+
+* **MoskalykA** - *Developer* - [MoskalykA](https://github.com/MoskalykA/) - *Part of the library*
+* **Vic92548** - *CTO* - [Vic92548](https://github.com/Vic92548) - *Part of the library*
